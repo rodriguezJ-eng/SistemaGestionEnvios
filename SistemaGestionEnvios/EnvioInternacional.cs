cc4c6554a35b3afb1492653dedbe358049d50aed
rodriguezJ-eng <alejandrodriguez0710@gmail.com>
2026-05-18 02:26:25 -0600

    clases abstractas y constructores implementados (vacíos)

--- SistemaGestionEnvios/EnvioInternacional.cs
+++ SistemaGestionEnvios/EnvioInternacional.cs
@@ -6,4 +6,18 @@ public class EnvioInternacional : Envio
     // definir atributos y propiedades
     // definir constructor
     // aplicar métodos heredados
+    public EnvioInternacional(string numeroPaquete, string remitente, string destinatario, string estado, DateTime fechaRegistro, Paquete paquete) : base(numeroPaquete, remitente, destinatario, estado, fechaRegistro, paquete)
+    {
+    }
+
+    
+    public override double CalcularCosto()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string TipoEnvio()
+    {
+        throw new NotImplementedException();
+    }
 }
