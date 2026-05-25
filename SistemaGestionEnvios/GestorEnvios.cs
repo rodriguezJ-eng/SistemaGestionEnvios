55a7f6bc277d72ae71d8ef297d753267ba9395fe
EngelAZ <engel.zeledon26@gmail.com>
2026-05-25 11:52:49 -0600

    Recreacion de la clase GestorEnvios diagramada

--- SistemaGestionEnvios/GestorEnvios.cs
+++ SistemaGestionEnvios/GestorEnvios.cs
@@ -1,9 +1,89 @@
-﻿// Clase que se encargara de la gestión de los envios
-// aquí se manejara la persistencia de datos
-// se hara cargo de hacer el registro de los envios
-// se hara cargo de mostrar los registros
-// se hara cargo de buscar los registros
-// se hara cargo de modificar los registros
-// se hara cargo de eliminar registros
-// se hara cargo de filtrar registros
-// se hara cargo de la persistencia de datos ( guardar / cargar )
+﻿
+public class GestorEnvios
+{
+    // Atributos
+    private List<Envio> _Envios;
+    private string _RutaEnvio;
+
+    // Constructor
+    public GestorEnvios(List<Envio> envios, string rutaEnvio)
+    {
+        Envios = envios;
+        RutaEnvio = rutaEnvio;
+    }
+
+    // propiedades
+    public List<Envio> Envios
+    {
+        get { return _Envios; }
+        set { _Envios = value; }
+    }
+
+    public string RutaEnvio
+    {
+        get { return _RutaEnvio; }
+        set { _RutaEnvio = value; }
+    }
+
+    // Metodos
+    public void RegistrarEnvio()
+    {
+
+    }
+
+    public List<T> MostrarEnvios()
+    {
+        return MostrarEnvios;
+    }
+
+    public Envio BuscarEnvio(int numeroGuia)
+    {
+        return BuscarEnvio(1);
+    }
+
+    public void ModificarEnvio(int numeroGuia, DateTime fechaEnvio)
+    {
+
+    }
+
+    public void ModificarEnvio(int numeroGuia, string origen)
+    {
+
+    }
+
+    public void ModificarEnvio(int numeroGuia, string destino)
+    {
+
+    }
+
+    public void ModificarEnvio(int numeroGuia)
+    {
+
+    }
+
+    public void ModificarEnvio(int numeroGuia, List<Paquete> paquetes)
+    {
+
+    }
+
+    public void ModificarEnvio(int numeroGuia, string tipoEnvio)
+    {
+
+    }
+
+    public void EliminarEnvio()
+    {
+
+    }
+
+    public void GuardarArchivo()
+    {
+
+    }
+
+    public void CargarArchivo()
+    {
+
+    }
+
+}
