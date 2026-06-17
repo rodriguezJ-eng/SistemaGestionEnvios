d0c089e9096955f0c126b909c08c4635af71cb4d
EngelAZ <engel.zeledon26@gmail.com>
2026-06-17 00:28:59 -0600

    Creacion de la clase UI_Alerta

--- SistemaGestionEnvios/View/UI_Validaciones/UI_Alerta.cs
+++ SistemaGestionEnvios/View/UI_Validaciones/UI_Alerta.cs
@@ -1,3 +1,118 @@
 public static class UI_Alerta
 {
+    public static void MostrarAdvertencia(string mensaje)
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("==============================");
+        Console.WriteLine("        ¡ADVERTENCIA!");
+        Console.WriteLine("==============================");
+
+        Console.ResetColor();
+        Console.WriteLine("\n==============================");
+        Console.WriteLine("Detalles de la advertencia:");
+        Console.WriteLine(mensaje);
+        Console.WriteLine("==============================");
+        UI_Sistema.UI_Pausa();
+    }
+
+
+    public static void MostrarAdvertencia(string mensaje, bool limpiar, bool pausar)
+    {
+        if (limpiar)
+        {
+            Console.Clear();
+        }
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("\n==============================");
+        Console.WriteLine("        ¡ADVERTENCIA!");
+        Console.WriteLine("==============================");
+
+        Console.ResetColor();
+        Console.WriteLine("\n==============================");
+        Console.WriteLine("Detalles de la advertencia:");
+        Console.WriteLine(mensaje);
+        Console.WriteLine("==============================");
+        if (pausar)
+        {
+            UI_Sistema.UI_Pausa();
+        }
+    }
+
+    public static void MostrarError(string mensaje)
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("==============================");
+        Console.WriteLine("    ¡HA OCURRIDO UN ERROR !");
+        Console.WriteLine("==============================");
+
+        Console.ResetColor();
+        Console.WriteLine("\n==============================");
+        Console.WriteLine("Detalles del error:");
+        Console.WriteLine(mensaje);
+        Console.WriteLine("==============================");
+        UI_Sistema.UI_Pausa();
+    }
+
+    public static void MostrarError(string mensaje, bool limpiar, bool pausar)
+    {
+        if (limpiar)
+        {
+            Console.Clear();
+        }
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("\n==============================");
+        Console.WriteLine("    ¡HA OCURRIDO UN ERROR !");
+        Console.WriteLine("==============================");
+
+        Console.ResetColor();
+        Console.WriteLine("\n==============================");
+        Console.WriteLine("Detalles del error:");
+        Console.WriteLine(mensaje);
+        Console.WriteLine("==============================");
+        if (pausar)
+        {
+            UI_Sistema.UI_Pausa();
+        }
+    }
+
+
+    public static void MostrarExito(string mensaje)
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("===================================");
+        Console.WriteLine("   ¡ACCIÓN REALIZADA CON ÉXITO!");
+        Console.WriteLine("===================================");
+
+        Console.ResetColor();
+        Console.WriteLine("\n===================================");
+        Console.WriteLine("Detalles:");
+        Console.WriteLine(mensaje);
+        Console.WriteLine("===================================");
+        UI_Sistema.UI_Pausa();
+    }
+
+    public static void MostrarExito(string mensaje, bool limpiar, bool pausar)
+    {
+        if (limpiar)
+        {
+            Console.Clear();
+        }
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\n===================================");
+        Console.WriteLine("   ¡ACCIÓN REALIZADA CON ÉXITO!");
+        Console.WriteLine("===================================");
+
+        Console.ResetColor();
+        Console.WriteLine("\n===================================");
+        Console.WriteLine("Detalles:");
+        Console.WriteLine(mensaje);
+        Console.WriteLine("===================================");
+        if (pausar)
+        {
+            UI_Sistema.UI_Pausa();
+        }
+    }
 }
